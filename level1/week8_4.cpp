d7b2258e09d94fef4c7eca4728158a6a9cd5c8ce
suzn2 <ryusuzn@gmail.com>
2021-06-22 15:27:04 +0900

    K번째 수

--- level1/week8_4.cpp
+++ level1/week8_4.cpp
@@ -27,3 +27,23 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     
     return answer;
 }
+
+//다른 사람 풀이
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> temp;
+
+    for(int i = 0; i < commands.size(); i++) {
+        temp = array;
+        sort(temp.begin() + commands[i][0] - 1, temp.begin() + commands[i][1]);
+        answer.push_back(temp[commands[i][0] + commands[i][2]-2]);
+    }
+
+    return answer;
+}

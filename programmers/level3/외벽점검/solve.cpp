a39e430b4424ba34d486d225a8dbc4f6150d4788
ryusuz <ryusuzn@gmail.com>
2022-03-21 19:04:26 +0900

    22.03.20

--- programmers/level3/외벽점검/solve.cpp
+++ programmers/level3/외벽점검/solve.cpp
@@ -1,4 +1,51 @@
-// 22.03.20. 외벽 점검
+// 22.03.20. 외벽 점검 (순열 + 원형탐색 배열 확장)
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int solution(int n, vector<int> weak, vector<int> dist) {
+    int answer = -1;
+    int W = weak.size();
+    int D = dist.size();
+    
+    sort(dist.begin(), dist.end());
+    
+    for(int i = 0; i < W; i++)
+        weak.push_back(weak[i] + n);
+    
+    for(int i = 0; i < D; i++) { // 투입한 친구
+        for(int j = 0; j < W; j++) { // 시작 지점 선택
+            vector<int> temp;
+        
+            for(int k = D - 1 - i; k < D; k++)
+                temp.push_back(dist[k]);
+            
+            do {
+                int idx = 0;
+                
+                for(int k = 0; k <= i; k++) { // 친구 투입
+                    int start = weak[j + idx] + temp[k];                   
+                    
+                    while(weak[j + idx + 1] <= start) idx++;
+            
+                    idx++;
+                }
+
+
+                if(idx >= W) return i + 1;
+                
+            }while(next_permutation(temp.begin(), temp.end()));
+        }
+    }
+    
+    return answer;
+}
+
+
+
+// 내 풀이 틀림
 #include <string>
 #include <vector>
 #include <iostream>
--- programmers/level3/외벽점검/README.md
+++ programmers/level3/외벽점검/README.md
@@ -5,5 +5,8 @@ https://programmers.co.kr/learn/courses/30/lessons/60062
 </br>
 
 ## ✏️ 풀이 방법
+순열을 이용해서 푸는 문제.
 
+dist가 긴 친구부터 하는게 유리!!!
 
+ 

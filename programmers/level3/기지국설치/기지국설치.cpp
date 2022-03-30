8b2787b80e5085234cc9afad3aca51c59a0b0d1f
ryusuz <ryusuzn@gmail.com>
2022-03-30 21:45:51 +0900

    22.03.28

---
+++ programmers/level3/기지국설치/기지국설치.cpp
@@ -0,0 +1,26 @@
+// 22.03.28. 기지국 설치
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(int n, vector<int> stations, int w)
+{
+    int answer = 0, start = 1, index = 0;
+
+    while(start <= n)
+    {
+        if(start >= stations[index] - w && start <= stations[index] + w)
+        {
+            start = stations[index] + w;
+            index++;
+        }
+        else
+        {
+            start += 2 * w;
+            answer++;
+        }
+        start++;
+    }
+
+    return answer;
+}

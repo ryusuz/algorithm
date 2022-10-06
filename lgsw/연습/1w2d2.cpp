1458270da6a8b6fc630044056c880838f5c687a8
ryusuz <ryusuzn@gmail.com>
2022-10-06 09:29:35 +0900

    22.10.05.

---
+++ lgsw/연습/1w2d2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int N;//빌딩의 층수
+int num[9]
+void InputData(){
+    cin >> N;
+}
+
+int solve() {
+    int var = N / 9;
+
+    
+
+    return 0;
+}
+
+int main(){
+    int ans = -1;
+    InputData();//입력받는 부분
+
+    //여기서부터 작성
+    ans = solve();
+
+    cout << ans << endl;//출력하는 부분
+    return 0;
+}
